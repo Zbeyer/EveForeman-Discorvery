837a6fa14630dcc9fa2df4b1de30e12bea732132
Zachary Beyer <Zachary@zbeyer.com>
2020-03-10 09:50:31 -0700

    Swap Legal and development on landing page

--- client/src/components/Root.vue
+++ client/src/components/Root.vue
@@ -8,18 +8,18 @@
     body="Eve Foreman intends to be an application to aide corporate mining operations and provide a suite of tools. The project is still in its early stages so please be patient as we get all our ducks in a row..."/>
 
     <content-card pic="https://vuejsexamples.com/favicon.png"
-    type="leadership"
-    title="Want To Help?"
-    subtitle="This is an open source project"
-    footnote="Propper Landing Pending"
-    body="Want to contribute? This is an open source project—-head on over to <a href='https://github.com/Zbeyer/EveForeman'>Github</a>!"/>
-
-    <content-card pic="https://vuejsexamples.com/favicon.png"
     type="gallente"
     title="Legal Acknowledgement"
     subtitle="Attribution and Acknowledgements"
     footnote="Covering our collective butts"
     body="This project and any any forks may be subject to <a href='https://developers.eveonline.com/resource/license-agreement'>CCP Developer License</a>. Use of static content and SSO login are intended (but not yet implemented)."/>
+
+    <content-card pic="https://vuejsexamples.com/favicon.png"
+    type="leadership"
+    title="Want To Help?"
+    subtitle="This is an open source project"
+    footnote="A league of developers"
+    body="Want to contribute? This is an open source project—-head on over to <a href='https://github.com/Zbeyer/EveForeman'>Github</a>!"/>
 
     </div>
 
