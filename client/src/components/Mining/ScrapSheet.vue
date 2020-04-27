7d2c44ca74fa24d469b3da54f42f123eebe6d8c9
Zachary Beyer <Zachary@zbeyer.com>
2020-04-26 18:11:24 -0700

    computed properties experiment: working

--- client/src/components/Mining/ScrapSheet.vue
+++ client/src/components/Mining/ScrapSheet.vue
@@ -1,19 +1,44 @@
 <template>
     <div>
+        <div>
+            {{ helloWorld }}
+        </div>
+
+        <v-card light align="center" style="padding 2em;">
+            <v-form v-model="valid">
+                <v-container>
+                    <v-layout>
+                        <v-text-field v-model="amount" :rules="numbers" :counter="10" label="Amount (m3)" required>
+                        </v-text-field>
+
+                        <v-text-field v-model="cycleTime" :rules="numbers" :counter="3" label="Cycle Time" required>
+                        </v-text-field>
+                        <v-label>
+                            {{ vps }}
+                        </v-label>
+                    </v-layout>
+                </v-container>
+            </v-form>
+        </v-card>
+
+        <div id="example">
+            <p>Original message: "{{ message }}"</p>
+            <p>Computed reversed message: "{{ reversedMessage }}"</p>
+        </div>
 
         <content-card pic="https://vuejsexamples.com/favicon.png" type="security" title="W.I.P." subtitle="Scrap Sheet"
-            footnote="Work in progress"
+            footnote="Work in progress" v-if="0"
             body="This page is a work in progress. While it may be visually interesting nothing yet is functional so please don't be too disappointed by aparent lack of functionality." />
 
-        <v-card light align="center">
+        <v-card light align="center" v-if="0">
             <v-sparkline :value="value" :gradient="gradient" :smooth="radius || false" :padding="padding"
                 :line-width="width" :stroke-linecap="lineCap" :gradient-direction="gradientDirection" :fill="fill"
                 :type="type" :auto-line-width="autoLineWidth" auto-draw />
             <v-form v-model="valid">
                 <div class="text-center">
-                    <span v-for="n in numbers">
+                    <!-- <span v-for="n in numbers">
                         <v-progress-circular :value="n" class="ma-1" color="teal" />
-                    </span>
+                    </span> -->
                 </div>
                 <v-calendar dark />
                 <v-container>
@@ -66,6 +91,7 @@
 </template>
 
 <script>
+    import Mining from '@/services/Mining'
     import ContentCard from '@/components/Generics/ContentCard'
     const gradients = [
         ['#222'],
@@ -79,6 +105,7 @@
         name: 'ScrapSheet',
         data () {
             return {
+                helloWorld: Mining.helloWorld(),
                 numbers: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
                 datePicker: '',
                 timePicker: '',
@@ -92,11 +119,24 @@
                 gradients,
                 fill: false,
                 type: 'trend',
-                autoLineWidth: false
+                autoLineWidth: false,
+                message: 'Hello',
+                amount: '',
+                cycleTime: ''
             }
         },
         components: {
             'content-card': ContentCard
+        },
+        computed: {
+            // a computed getter
+            reversedMessage: function () {
+                // `this` points to the vm instance
+                return this.message.split('').reverse().join('')
+            },
+            vps: function () {
+                return Mining.volumePerSecond(this.amount, this.cycleTime)
+            }
         }
 
     }
